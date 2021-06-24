b7fdb5263650dbefcc46c003d72e3bb9a26b3a6c
kyomin <kim031504@naver.com>
2021-06-24 18:07:05 +0900

    간단히 라우터 직접 만들기

--- src/main.js
+++ src/main.js
@@ -1,13 +1,25 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
 
 Vue.config.productionTip = false
+
+const Login = { template: '<div>Login Page</div>' }
+
+const routes = {
+  '/': App,
+  '/login': Login
+}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  computed: {
+    VueComponent () {
+      return routes[window.location.pathname] || { template: '<div>Page not found</div>' }
+    }
+  },
+  render (h) {
+    return h(this.VueComponent)
+  }
 })
