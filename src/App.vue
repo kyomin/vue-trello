428e185b8bddaa71f5d1ab43f8049cbeefb9af4b
kyomin <kim031504@naver.com>
2021-06-28 23:20:23 +0900

    라우터 링크 적용

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    {{ msg }}
+    <Navbar />
     <!-- 라우터 경로에 따라 바뀔 레이아웃이 여기에 삽입된다. -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
+
 export default {
   name: 'app',
+  components: { Navbar },
   data () {
     return {
-      msg: '여기서부터 코드를 시작합니다!'
+
     }
   }
 }
