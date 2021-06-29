9cca30a85df42be10ca4868c9c1fb523ac4b83f0
kyomin <kim031504@naver.com>
2021-06-29 21:17:57 +0900

    중첩 라우트 적용하기

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
-import NotFound from '../components/NotFound.vue'
 import Board from '../components/Board.vue'
+import Card from '../components/Card.vue'
+import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -12,7 +13,15 @@ const router = new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/login', component: Login },
-    { path: '/b/:bid', component: Board },
+    {
+      path: '/b/:bid',
+      component: Board,
+      // 중첩 라우팅. 위의 경로 하위에 적용되는 컴포넌트이다.
+      // 때문에, 이 컴포넌트가 그려질 부분을 router-view 태그로 상위 라우팅의 컴포넌트에서 지정해줘야 한다.
+      children: [
+        { path: 'c/:cid', component: Card }
+      ]
+    },
     { path: '*', component: NotFound }
   ]
 })
