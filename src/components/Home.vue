83ab6c00bcf59a5b9bbdade9841c1e704d1e0478
kyomin <kim031504@naver.com>
2021-06-29 22:10:57 +0900

    HttpXMLRequest 객체를 이용한 요청

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,10 @@
       Home
       <div>
         Board List:
+        <div v-if="loading">Loading...</div>
+        <div v-else>
+          API result: {{ apiRes }}
+        </div>
         <ul>
           <li>
             <router-link to="/b/1">Board 1</router-link>
@@ -17,7 +21,35 @@
 
 <script>
 export default {
+  data () {
+    return {
+      loading: false,
+      apiRes: ''
+    }
+  },
+  created () {
+    console.log('Home component created')
+    this.fetchData()
+  },
+  methods: {
+    fetchData () {
+      console.log('fetchData function called!')
+      this.loading = true
 
+      const req = new XMLHttpRequest()
+
+      req.open('GET', 'http://localhost:3000/health')
+      req.send()
+      req.addEventListener('load', () => {
+        this.loading = false
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response)
+        }
+      })
+    }
+  }
 }
 </script>
 
