a7d4e44230311aaf678c0ddbaff87e97f44d8b26
kyomin <kim031504@naver.com>
2021-06-30 18:28:43 +0900

    네비게이션 가드 적용

--- src/router/index.js
+++ src/router/index.js
@@ -8,18 +8,29 @@ import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
 
+const requireAuth = (to, from, next) => {
+  console.log('to : ', to)
+  console.log('from : ', from)
+  console.log('next : ', next)
+
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
+    { path: '/', component: Home, beforeEnter: requireAuth },
     { path: '/login', component: Login },
     {
       path: '/b/:bid',
       component: Board,
+      beforeEnter: requireAuth,
       // 중첩 라우팅. 위의 경로 하위에 적용되는 컴포넌트이다.
       // 때문에, 이 컴포넌트가 그려질 부분을 router-view 태그로 상위 라우팅의 컴포넌트에서 지정해줘야 한다.
       children: [
-        { path: 'c/:cid', component: Card }
+        { path: 'c/:cid', component: Card, beforeEnter: requireAuth }
       ]
     },
     { path: '*', component: NotFound }
